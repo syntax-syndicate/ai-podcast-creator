1fa0d683f811b092ae85fb23d38606b78d046bc9
iboughtbed <testpythontosanzhar@gmail.com>
2025-01-07 00:34:53 +0500

    refactor: change site config url

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,8 +7,8 @@ const links = {
 
 export const siteConfig = {
   name: "Parallax Podcast",
-  url: "https://iboughtbed-ai-podcast.vercel.app",
-  ogImage: "https://iboughtbed-ai-podcast.vercel.app/og.png",
+  url: "https://prllx.vercel.app",
+  ogImage: "https://prllx.vercel.app/og.png",
   description: "Daily Interactive AI Podcast",
   links,
   mainNav: [
