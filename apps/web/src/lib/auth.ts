5f750473c89a0da61c2ce709ea297b8143e7f24a
iboughtbed <testpythontosanzhar@gmail.com>
2025-03-29 00:24:47 +0500

    fix: add trusted origins

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -24,4 +24,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
+  trustedOrigins: process.env.BETTER_AUTH_TRUSTED_ORIGIN?.split(",") ?? [],
 });
