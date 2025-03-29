9db25a7480dd22049c45d008e165801bacb003f6
iboughtbed <testpythontosanzhar@gmail.com>
2025-03-30 02:21:28 +0500

    chore: remove/rename

--- apps/web/src/lib/kv.ts
+++ apps/web/src/lib/kv.ts
@@ -2,7 +2,7 @@ import { Redis } from "@upstash/redis";
 
 import { env } from "@/env";
 
-export const redis = new Redis({
+export const kv = new Redis({
   url: env.UPSTASH_REDIS_REST_URL,
   token: env.UPSTASH_REDIS_REST_TOKEN,
 });
--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
-import { customAlphabet } from "nanoid";
+// import { customAlphabet } from "nanoid";
 import { twMerge } from "tailwind-merge";
 
 import { env } from "@/env";
@@ -8,28 +8,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function generateId(length = 20) {
-  return customAlphabet(
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-    length,
-  )();
-}
-
-export function getFormattedDate() {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, "0");
-  const day = String(today.getDate()).padStart(2, "0");
-
-  return `${day}.${month}.${year}`;
-}
-
 export function absoluteUrl(path: string) {
   return `${env.NEXT_PUBLIC_APP_URL}${path}`;
-}
-
-export function isMacOs() {
-  if (typeof window === "undefined") return false;
-
-  return window.navigator.userAgent.includes("Mac");
 }
