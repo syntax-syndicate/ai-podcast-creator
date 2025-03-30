451a11b83d57c26c7645dc5e9a6b61f851ec6914
iboughtbed <testpythontosanzhar@gmail.com>
2025-03-31 00:39:27 +0500

    fix: set tailwind config in prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 /** @type {import("prettier").Config & import("prettier-plugin-tailwindcss").PluginOptions} */
 const config = {
+  tailwindConfig: "./apps/web/tailwind.config.ts",
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
